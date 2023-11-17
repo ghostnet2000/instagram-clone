fb1a8ce3885ce5cb426d1e360bff413ccb8b369a
Viwe Lolwane <viwelolwane@gmail.com>
2023-11-17 20:40:52 +0200

    add stories

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ class InstagramApp(MDApp):
         Builder.load_file('libs/screens/home_page.kv')
         Builder.load_file('libs/components/appbar.kv')
         Builder.load_file('libs/components/bottom_nav.kv')
+        Builder.load_file('libs/components/story_creator.kv')
+        Builder.load_file('libs/components/circular_avatar_image.kv')
+
+    def on_start(self):
+        self.root.dispatch('on_enter')
 
 
 if __name__ == "__main__":
